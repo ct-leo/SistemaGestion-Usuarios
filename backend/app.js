e0f0f578146e860d5f160ce0dd15da42d989d9c9
Leonel ツ <tarrillo9999@gmail.com>
2026-05-04 01:31:05 -0500

    Configure correct routes to serve the frontend from the backend in production

--- backend/app.js
+++ backend/app.js
@@ -15,12 +15,12 @@ function createApp() {
 
   app.use("/usuarios", usuariosRouter);
 
-  app.use(express.static(path.join(__dirname, "../../dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../dist/index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
-  
+
   app.use((req, res) => {
     res.status(404).json({ error: "Ruta no encontrada" });
   });
